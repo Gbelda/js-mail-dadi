5449f0cba505e9df535422d6dcf4005b99a11f1c
gbelda <gbelda56@gmail.com>
2021-10-14 14:00:06 +0200

    Linked HTML to CSS and JS files

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,5 +1,34 @@
+/* 
+
+Strumenti:
+Loop
+If / Else If / Else
+math.random
+eventListener
+Array
+
+*/
+
+
+
 //Generare gli elementi da utilizzare
+
 //Generare un array di email
+const emails = [
+    'boolean1@gmail.com',
+    'boolean2@gmail.com',
+    'boolean3@gmail.com',
+    'boolean4@gmail.com',
+    'boolean5@gmail.com',
+]
+
+for (let n = 0; n < emails.length; n++) {
+    const userEmail = emails[n];
+    console.log(userEmail);
+}
+
+
+
 //Assegnare al bottone login un eventListener
 //Far confrontare email input dalle email del array
 //Se non e uguale, far uscire un Alert che l'email e invalido
