f6fdb587465a41163d78af3d1ed473b1687c44b8
gbelda <gbelda56@gmail.com>
2021-10-14 13:50:37 +0200

    Added JS tasks to complete

---
+++ assets/JS/main.js
@@ -0,0 +1,10 @@
+//Generare gli elementi da utilizzare
+//Generare un array di email
+//Assegnare al bottone login un eventListener
+//Far confrontare email input dalle email del array
+//Se non e uguale, far uscire un Alert che l'email e invalido
+//Se e uguale ad uno delle email del array allora far accedere al gioco
+//Assegnare al bottone ROLL DICE una formula Math.random a elemento User e elemento PC
+//Far confrontare i due valori
+//Se User > di PC mostrare CONGRATULATIONS YOU WIN!
+//Se PC > User mostrare YOU LOST...
